081fc4fe563123bd971c543d8f0285662769a5c5
Beatriz Bueno <Fbuenoalmagro@gmail.com>
2023-07-18 17:18:28 +0200

    initial commit

---
+++ src/main/java/dev/beabueno/wisteriammo/WisteriaMMO.java
@@ -0,0 +1,18 @@
+package dev.beabueno.wisteriammo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WisteriaMMO extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
