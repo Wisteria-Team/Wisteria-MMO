473e47eb5d0a7dc2d0dc40f6edf0ccf566a4457d
Pablo Garcia de los Salmones Valencia <DevPGSV@gmail.com>
2023-08-10 09:31:33 +0200

    Added basic configuration

--- src/main/java/dev/beabueno/wisteriammo/WisteriaMMO.java
+++ src/main/java/dev/beabueno/wisteriammo/WisteriaMMO.java
@@ -7,16 +7,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WisteriaMMO extends JavaPlugin {
 
+    private static WisteriaMMO plugin = null;
+
     @Override
     public void onEnable() {
         System.out.println("[WMMO] onEnable");
+        WisteriaMMO.plugin = this;
         Bukkit.getPluginManager().registerEvents(new TestEvents(), this);
         getCommand("wmmo").setExecutor(new WMMOCommand());
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
         System.out.println("[WMMO] enabled");
     }
 
     @Override
     public void onDisable() {
         System.out.println("[WMMO] onDisable");
+    }
+
+    public static WisteriaMMO getPluginInstance() {
+        return WisteriaMMO.plugin;
     }
 }
--- src/main/java/dev/beabueno/wisteriammo/commands/WMMOCommand.java
+++ src/main/java/dev/beabueno/wisteriammo/commands/WMMOCommand.java
@@ -1,19 +1,31 @@
 package dev.beabueno.wisteriammo.commands;
 
+import dev.beabueno.wisteriammo.WisteriaMMO;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class WMMOCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
 
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
             player.sendMessage("CMD executed");
+        } else {
+            System.out.println("Console cmd");
+        }
 
+        if (args.length >= 1) {
+            if (args[0].equals("reload")) {
+                FileConfiguration config = WisteriaMMO.getPluginInstance().getConfig();
+                ConfigurationSection a = config.getConfigurationSection("ability.quick_sword_attack");
+                System.out.println(a.getString("name"));
+            }
         }
 
         return false;
